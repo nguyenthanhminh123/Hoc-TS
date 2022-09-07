057acb4d303bfff6a8d4488c091495d1984620af
ThanhMinh <thanhminh339968az@gmail.com>
2022-09-07 21:14:57 +0700

    add

---
+++ Bai-09/src/lectures/04.main.ts
@@ -0,0 +1,24 @@
+// Dùng cho hằng số UPPER_CASE
+const API_URL = 'https://api.com';
+const BACKROUND_URL = 'https://image.com'
+
+const DEFAULT_SIZE = 12;
+const SECONDS_PER_HOUR = 3600;
+
+// Dùng cho biến không có nhu cầu thay đổi giá chị camelCase
+const channelName = 'Easy Frontend';
+const studentId = 123;
+const isActive = true;
+const student = {};
+student.name = 'Bob';
+const numberList = [];
+numberList.push(1);
+ 
+// dùng cho những biến có nhu cầu thay đổi bằng việc thực hiện phép gán (re-assignment)
+let count = 1;
+count++;
+for (let i = 0; i < 10; i++) {
+console.log(i);
+}
+let numberLists = [1, 2, 3];
+numberLists = [];
